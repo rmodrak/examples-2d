6bf10491ba664c6f06d57bd3529aa1bc5996abed
Ryan Modrak <rmodrak@lanl.gov>
2020-05-19 12:50:37 -0600

    First commit

---
+++ paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = '/u1/uaf/rmodrak/data/2dAcoustic/checkers/model_init'
+
+MODEL_TRUE = '/u1/uaf/rmodrak/data/2dAcoustic/checkers/model_true'
+
+PRECOND = ''
+
+SPECFEM_DATA = '/u1/uaf/rmodrak/data/2dAcoustic/checkers/specfem2d-d745c542/DATA'
+
+SPECFEM_BIN = '/u1/uaf/rmodrak/data/2dAcoustic/checkers/specfem2d-d745c542/bin'
+
